8cfa6af069a147b7a43351e416335bf594feb370
Joseph <mr.levs13@gmail.com>
2024-02-09 14:42:30 +0100

    suppression fichier inutile, création des pages avec l'objectif du contenue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/MyPage">My Page</router-link> |
+    <router-link to="/ErrorPage">Erreur 404</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p> Votre nom et prénom <br/>
+      • Une courte présentation<br/>
+      • Vos créations avec à chaque fois une image et un titre. Une modal s'ouvrira avec plus<br/>
+      de photos et détails au clic. La disposition est libre.<br/>
+      • Un formulaire de contact (nom/prénom, objet et message) qui s'envoie à l'adresse email définie en variable d'environnement.
+      
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
