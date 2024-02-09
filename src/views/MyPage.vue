8c7d187934f087a160d126103fe15070ab22db1c
Joseph <mr.levs13@gmail.com>
2024-02-09 14:29:41 +0100

    suppression fichier inutile, création des pages avec l'objectif du contenue

---
+++ src/views/MyPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>• Le titre. <br/>
+        • La date de création.<br/>
+        • Les technologies utilisées.<br/>
+        • Un lien pour visiter le site ou le fichier PDF selon le projet (dans un nouvel onglet).<br/>
+        • Un lien vers le repository GitHub le cas échéant.<br/>
+        • La modal se fermera lorsqu'on clique sur le bouton de fermeture ou en dehors. Elle
+          devra rester fixe sur la page même si on scrolle.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+    name:'MyPage',
+    components: { }
+
+}
+</script>
+
+<style>
+
+</style>
